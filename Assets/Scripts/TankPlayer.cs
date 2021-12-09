281a31e3c0ac34849e87a882de088e8a2027b0ee
AZ366098 <84701052+Andy366098@users.noreply.github.com>
2021-12-09 19:45:46 +0800

    創建坦克到42頁
    已可在另一程式上顯示別人的坦克

--- Assets/Scripts/TankPlayer.cs
+++ Assets/Scripts/TankPlayer.cs
@@ -1,18 +1,24 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankPlayer : MonoBehaviour
+public class TankPlayer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Complete.TankMovement m_Movement;
+    private Complete.TankShooting m_Shooting;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        m_Movement = GetComponent<Complete.TankMovement>(); //獲得物件上的Component
+        m_Shooting = GetComponent<Complete.TankShooting>();
+
+        if (!photonView.IsMine) //如果不是我自己的就不能操控
+        {
+            m_Movement.enabled = false;
+            m_Shooting.enabled = false;
+
+            enabled = false;
+        }
     }
 }
